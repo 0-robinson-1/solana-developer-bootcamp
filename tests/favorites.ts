f33aea2d8a26efae0ec9bca9787277887dfea342
0.RobinSon.1 <0.robinson.1@protonmail.com>
2025-08-30 18:03:54 +0200

    Writing test for project 1 favorites

---
+++ tests/favorites.ts
@@ -0,0 +1,34 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { Favorites } from "../target/types/favorites";  // Path to generated types
+import { expect } from 'chai';
+
+describe("favorites", () => {
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+
+  const user = provider.wallet;
+  const program = anchor.workspace.Favorites as Program<Favorites>;
+
+  const expectedNumber = 8;
+  const expectedColor = "green";
+  const expectedHobbies = ["coding", "Adventuring"];
+
+  it("Sets and gets favorites!", async () => {
+    await program.methods
+      .setFavorites(expectedNumber, expectedColor, expectedHobbies)
+      .accountsPartial({ user: user.publicKey })
+      .rpc();
+
+    const favoritesPda = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("favorites"), user.publicKey.toBuffer()],
+      program.programId
+    )[0];
+
+    const favoritesAccount = await program.account.favorites.fetch(favoritesPda);
+
+    expect(favoritesAccount.number).to.equal(expectedNumber);
+    expect(favoritesAccount.color).to.equal(expectedColor);
+    expect(favoritesAccount.hobbies).to.deep.equal(expectedHobbies);
+  });
+});
