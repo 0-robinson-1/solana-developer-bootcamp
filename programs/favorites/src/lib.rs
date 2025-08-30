ac23ce970275996aa97de2cdf0b8aab6d39119a3
0.RobinSon.1 <0.robinson.1@protonmail.com>
2025-08-30 16:20:47 +0200

    project 1 favorites

--- programs/favorites/src/lib.rs
+++ programs/favorites/src/lib.rs
@@ -3,3 +3,40 @@ use anchor_lang::prelude::*;
 declare_id!("AEvRM2qnZZoQa7zVg1wbWjkyH4Ls4vxp4TRM31QKT8W9");
 
 pub const ANCHOR_DISCRIMINATOR: usize = 8;
+
+#[program]
+pub mod favorites {
+    use super::*;
+
+    pub fn set_favorites() -> Return<()> {
+        msg!("Greetings from {}", context.program_id);
+    }
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Favorites {
+    pub number: u64,
+
+    #[max_len(50)]
+    pub color: String,
+
+    #[max_len(5, 50)]
+    pub hobbies: Vec<String>,
+}
+
+pub struct SetFavorites<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        init_if_needed,
+        payer = user,
+        space = ANCHOR_DISCRIMINATOR + Favorites::INIT_SPACE,
+        seeds = [b"favorites", user.key().as_ref()],
+        bump
+    )]
+    pub favorites: Account<'info, Favorites>,
+
+    pub system_program: Program<'info, System>,
+}
