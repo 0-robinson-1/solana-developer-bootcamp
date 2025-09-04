dceaba55cf8b206a5f5eb1c5e5d17435fefb1ab2
0.RobinSon.1 <0.robinson.1@protonmail.com>
2025-09-04 14:28:51 +0200

    writing tests

--- voting-dapp/anchor/tests/votingdapp.test.ts
+++ voting-dapp/anchor/tests/votingdapp.test.ts
@@ -21,125 +21,27 @@ import { loadKeypairSignerFromFile } from 'gill/node'
 
 const { rpc, sendAndConfirmTransaction } = createSolanaClient({ urlOrMoniker: process.env.ANCHOR_PROVIDER_URL! })
 
+import { BankrunProvider, startAnchor } from "anchor-bankrun";
+
+import { Votingdapp } from '../target/types/votingdapp.ts';
+const IDL = require('../target/idl/votingdapp.json');
+
+const votingDappAddress = new PublicKey("JAVuBXeBZqXNtS73azhBDAoYaaAFfo4gWXoZe2e7Jf8H");
+
 describe('votingdapp', () => {
-  let payer: KeyPairSigner
-  let votingdapp: KeyPairSigner
 
-  beforeAll(async () => {
-    votingdapp = await generateKeyPairSigner()
-    payer = await loadKeypairSignerFromFile(process.env.ANCHOR_WALLET!)
-  })
+  it('Initialize Poll', async () => {
+    const context = await startAnchor("", [{name: "votingDapp", programId: votingDappAddress}], []);
+    const provider = new BankrunProvider(context);
 
-  it('Initialize Votingdapp', async () => {
-    // ARRANGE
-    expect.assertions(1)
-    const ix = getInitializeInstruction({ payer: payer, votingdapp: votingdapp })
+    const votingProgram = new Program<VotingDapp>(
+      IDL,
+      provider,
+    );
 
-    // ACT
-    await sendAndConfirm({ ix, payer })
+    await votingProgram.methods.initializePoll(
 
-    // ASSER
-    const currentVotingdapp = await fetchVotingdapp(rpc, votingdapp.address)
-    expect(currentVotingdapp.data.count).toEqual(0)
-  })
-
-  it('Increment Votingdapp', async () => {
-    // ARRANGE
-    expect.assertions(1)
-    const ix = getIncrementInstruction({
-      votingdapp: votingdapp.address,
-    })
-
-    // ACT
-    await sendAndConfirm({ ix, payer })
-
-    // ASSERT
-    const currentCount = await fetchVotingdapp(rpc, votingdapp.address)
-    expect(currentCount.data.count).toEqual(1)
-  })
-
-  it('Increment Votingdapp Again', async () => {
-    // ARRANGE
-    expect.assertions(1)
-    const ix = getIncrementInstruction({ votingdapp: votingdapp.address })
-
-    // ACT
-    await sendAndConfirm({ ix, payer })
-
-    // ASSERT
-    const currentCount = await fetchVotingdapp(rpc, votingdapp.address)
-    expect(currentCount.data.count).toEqual(2)
-  })
-
-  it('Decrement Votingdapp', async () => {
-    // ARRANGE
-    expect.assertions(1)
-    const ix = getDecrementInstruction({
-      votingdapp: votingdapp.address,
-    })
-
-    // ACT
-    await sendAndConfirm({ ix, payer })
-
-    // ASSERT
-    const currentCount = await fetchVotingdapp(rpc, votingdapp.address)
-    expect(currentCount.data.count).toEqual(1)
-  })
-
-  it('Set votingdapp value', async () => {
-    // ARRANGE
-    expect.assertions(1)
-    const ix = getSetInstruction({ votingdapp: votingdapp.address, value: 42 })
-
-    // ACT
-    await sendAndConfirm({ ix, payer })
-
-    // ASSERT
-    const currentCount = await fetchVotingdapp(rpc, votingdapp.address)
-    expect(currentCount.data.count).toEqual(42)
-  })
-
-  it('Set close the votingdapp account', async () => {
-    // ARRANGE
-    expect.assertions(1)
-    const ix = getCloseInstruction({
-      payer: payer,
-      votingdapp: votingdapp.address,
-    })
-
-    // ACT
-    await sendAndConfirm({ ix, payer })
-
-    // ASSERT
-    try {
-      await fetchVotingdapp(rpc, votingdapp.address)
-    } catch (e) {
-      if (!isSolanaError(e)) {
-        throw new Error(`Unexpected error: ${e}`)
-      }
-      expect(e.message).toEqual(`Account not found at address: ${votingdapp.address}`)
-    }
-  })
-})
-
-// Helper function to keep the tests DRY
-let latestBlockhash: Awaited<ReturnType<typeof getLatestBlockhash>> | undefined
-async function getLatestBlockhash(): Promise<Readonly<{ blockhash: Blockhash; lastValidBlockHeight: bigint }>> {
-  if (latestBlockhash) {
-    return latestBlockhash
-  }
-  return await rpc
-    .getLatestBlockhash()
-    .send()
-    .then(({ value }) => value)
-}
-async function sendAndConfirm({ ix, payer }: { ix: Instruction; payer: KeyPairSigner }) {
-  const tx = createTransaction({
-    feePayer: payer,
-    instructions: [ix],
-    version: 'legacy',
-    latestBlockhash: await getLatestBlockhash(),
-  })
-  const signedTransaction = await signTransactionMessageWithSigners(tx)
-  return await sendAndConfirmTransaction(signedTransaction)
-}
+    );
+    
+  });
+});
